13808ad2de6bfa1894fa8a7f85c183ddaab0141d
anhaidgroup <anhaidgroup@gmail.com>
2016-06-09 09:48:07 -0500

    Update affine.py

--- py_stringmatching/similarity_measure/affine.py
+++ py_stringmatching/similarity_measure/affine.py
@@ -14,7 +14,10 @@ def sim_ident(char1, char2):
 
 
 class Affine(SequenceSimilarityMeasure):
-    """Affine similarity measure class.
+    """Computes the affine gap score between two strings.
+
+    The affine gap measure is an extension of the Needleman-Wunsch measure that handles the longer gaps more
+    gracefully. For more information refer to the rsing matching chapter in the DI book.
 
     Parameters:
         gap_start (float): Cost for the gap at the start (defaults to 1)
@@ -30,12 +33,7 @@ class Affine(SequenceSimilarityMeasure):
 
     def get_raw_score(self, string1, string2):
         """
-        Computes the Affine gap score between two strings.
-
-        The Affine gap measure is an extension of the Needleman-Wunsch measure that handles the longer gaps more
-        gracefully.
-
-        For more information refer to string matching chapter in the DI book.
+        
 
         Args:
             string1,string2 (str) : Input strings
