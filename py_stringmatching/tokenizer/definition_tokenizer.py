59b05b69e76c86ab5d8832112b1eb0e5d2026375
anhaidgroup <anhaidgroup@gmail.com>
2016-06-08 22:00:31 -0500

    Update definition_tokenizer.py

--- py_stringmatching/tokenizer/definition_tokenizer.py
+++ py_stringmatching/tokenizer/definition_tokenizer.py
@@ -4,7 +4,9 @@ from py_stringmatching.tokenizer.tokenizer import Tokenizer
 
 
 class DefinitionTokenizer(Tokenizer):
-    """A class of tokenizers that uses a definition (e.g., alphabetical, alphanumeric, qgram) to find tokens, as opposed to using delimiters (e.g., whitespace). 
+    """A class of tokenizers that uses a definition to find tokens, as opposed to using delimiters.
+    
+    Examples of definitions include alphabetical tokens, qgram tokens. Examples of delimiters include white space, punctuations.
 
     Parameters:
         return_set (boolean): An attribute which is a flag to indicate whether to return a set of
