02ca2c89ec3f523edab68eef85c0e1f0302b056d
anhaidgroup <anhaidgroup@gmail.com>
2016-06-08 21:41:33 -0500

    Update tokenizer.py

--- py_stringmatching/tokenizer/tokenizer.py
+++ py_stringmatching/tokenizer/tokenizer.py
@@ -4,27 +4,25 @@ class Tokenizer(object):
     """Tokenizer class.
 
     Parameters:
-        return_set (boolean): flag to indicate whether to return a set of
-                              tokens. (defaults to False) 
+        return_set (boolean): an attribute which is a flag to indicate whether to return a set of
+                              tokens instead of a bag of tokens (defaults to False). 
     """
     def __init__(self, return_set=False):
         self.return_set = return_set
 
     def get_return_set(self):
-        """
-        Get the return_set flag
+        """Get the value of the return_set flag.
 
         Returns:
-            return_set (boolean)
+            The boolean value of the return_set flag. 
         """
         return self.return_set
 
     def set_return_set(self, return_set):
-        """
-        Set the return_set flag
+        """Set the value of the return_set flag.
 
         Args:
-            return_set (boolean): flag to indicate whether to return a set of tokens. 
+            return_set (boolean): a flag to indicate whether to return a set of tokens instead of a bag of tokens.
         """
         self.return_set = return_set
         return True
