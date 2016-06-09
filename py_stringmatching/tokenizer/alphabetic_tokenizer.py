276c52a25eea93e683a02794cef87b53b486f92c
anhaidgroup <anhaidgroup@gmail.com>
2016-06-09 14:24:38 -0500

    Updated alphabetic_tokenizer.py

--- py_stringmatching/tokenizer/alphabetic_tokenizer.py
+++ py_stringmatching/tokenizer/alphabetic_tokenizer.py
@@ -11,7 +11,7 @@ class AlphabeticTokenizer(DefinitionTokenizer):
         return_set (boolean): A flag to indicate whether to return 
         a set of tokens instead of a bag of tokens (defaults to False).
         
-    Public Attributes: 
+    Attributes: 
         return_set (boolean): An attribute that stores the value for the flag return_set. 
     """
     
