8a03c5d998f3abf7a347078d64cf0c878ebf1354
srujithpoondla <spoondla@wisc.edu>
2017-06-23 16:50:35 -0500

    Modified comments in cython_monge_elkon.pyx

--- py_stringmatching/similarity_measure/cython/cython_monge_elkan.pyx
+++ py_stringmatching/similarity_measure/cython/cython_monge_elkan.pyx
@@ -8,6 +8,13 @@ cimport numpy as np
 from py_stringmatching.similarity_measure.cython.cython_jaro_winkler import jaro_winkler
 
 def monge_elkan(bag1, bag2, sim_func, jw_flag=False):
+    """Cython function to return the similarity score for the given two bags of words"
+    Args:
+        bag1, bag2 (lists): Input lists.
+        sim_func (function): If jw_flag is set then the sim_func is Jaro Winkler else sim_func is  user defined.
+    Returns:
+        returns the Monge Elkan similarity score (float).
+    """
     cdef float sum_of_maxes = 0.0
     cdef float float_neg_inf = -float("inf")
     cdef float max_sim = 0.0
@@ -21,6 +28,8 @@ def monge_elkan(bag1, bag2, sim_func, jw_flag=False):
 
     if len1 == 0 or len2 == 0:
         return 0
+
+    # Check the jw_flag to get the sim_func
     if not jw_flag:
         for el1 in bag1:
             max_sim = float_neg_inf
