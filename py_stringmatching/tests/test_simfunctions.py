7693fb81febd919dd3e9e0cea2cd0f155e5e661f
srujithpoondla <spoondla@wisc.edu>
2017-06-29 19:04:07 -0500

    Added the global variable in Monge Elkan rounded test cases

--- py_stringmatching/tests/test_simfunctions.py
+++ py_stringmatching/tests/test_simfunctions.py
@@ -1971,7 +1971,7 @@ class MongeElkanTestCases(unittest.TestCase):
         fn2 = NeedlemanWunsch().get_raw_score
         me = MongeElkan(sim_func=fn1)
         self.assertEqual(me.get_sim_func(), fn1)
-        self.assertAlmostEqual(me.get_raw_score(round(
+        self.assertAlmostEqual(round(me.get_raw_score(
                 ['Comput.', 'Sci.', 'and', 'Eng.', 'Dept.,', 'University', 'of', 'California,', 'San', 'Diego'],
                 ['Department', 'of', 'Computer', 'Science,', 'Univ.', 'Calif.,', 'San', 'Diego']),
             NUMBER_OF_DECIMAL_PLACES), round(0.8364448051948052, NUMBER_OF_DECIMAL_PLACES))
