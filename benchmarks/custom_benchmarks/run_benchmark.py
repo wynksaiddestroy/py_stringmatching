9b9aa9d365dc154036866ee1d141d574af15d268
Paul <paulgc@cs.wisc.edu>
2017-07-17 19:41:34 -0500

    [BENCH] Add benchmark datasets. Clean up code.

--- benchmarks/custom_benchmarks/run_benchmark.py
+++ benchmarks/custom_benchmarks/run_benchmark.py
@@ -6,14 +6,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def run_benchmark(short_dataset, medium_dataset, long_dataset,
+def run_benchmark(short_dataset_path, medium_dataset_path, long_dataset_path,
                   data_size, sim_measure, tokenizer = None, num_repeat = 1, 
-                  random_seed = 0, output_file = None):
+                  random_seed = 0, output_file = None, encoding = 'latin-1'):
+    """Run benchmark for 9 configurations (short-short, short-medium, 
+    short-long, medium-short, medium-medium, medium-long, long-short, 
+    long-medium, long-long) for the provided similarity measure.
 
+    Specifically, this method will take in 3 files as input each containing 
+    one column of strings. Next, it will sample the input files based on the 
+    provided data_size and then runs benchmark for different configurations for 
+    the provided similarity measure. Finally, it returns a dataframe containing
+    the benchmark results.                     
+                                                                                
+    Args:                                                                   
+        short_dataset_path (string): Path to the dataset containing short strings.
+        medium_dataset_path (string): Path to the dataset containing medium strings.
+        long_dataset_path (string): Path to the dataset containing long strings.
+        data_size (int): Number of string pairs to be benchmarked.
+        sim_measure (function): Similarity function to be benchmarked.
+        tokenizer (function): Tokenizer to be used (in case of token-based similarity measures). Defaults to None.
+        num_repeat (int): Number of times to run each configuration. Defaults to 1.
+        random_seed (int): Random seed to be used for sampling. Defaults to 0.
+        output_file (string): Output path to save the benchmark results. Defaults to None.         
+        encoding (string): Encoding of the input datasets. Defaults to latin-1.
+
+    Returns:                                                                
+        Benchmark results (Dataframe).                                   
+                                                                                
+    Examples:
+        >>> jac = Jaccard()                                                 
+        >>> ws = WhitespaceTokenizer(return_set=True)
+        >>> results = run_benchmark('datasets/short_strings.csv', 'datasets/medium_strings.csv', 'datasets/long_strings.csv', 100000, 
+                jac.get_sim_score, ws.tokenize, output_file = 'result.csv') # Benchmark results will be saved in result.csv
+        >>> ed = Levenshtein()
+        >>> results = run_benchmark('datasets/short_strings.csv', 'datasets/medium_strings.csv', 'datasets/long_strings.csv', 100000,
+                      ed.get_sim_score) 
+    """
+  
     # read data
-    short_strings = pd.read_csv(short_dataset)
-    medium_strings = pd.read_csv(medium_dataset)                                  
-    long_strings = pd.read_csv(long_dataset)                                  
+    short_strings = pd.read_csv(short_dataset_path, encoding = encoding)
+    medium_strings = pd.read_csv(medium_dataset_path, encoding = encoding)                                  
+    long_strings = pd.read_csv(long_dataset_path, encoding = encoding)                                  
 
     short_len = len(short_strings)
     medium_len = len(medium_strings)
@@ -21,7 +55,7 @@ def run_benchmark(short_dataset, medium_dataset, long_dataset,
 
     # compute individual table size
     table_size = ceil(sqrt(data_size))
-    print(table_size)
+
     # sample strings    
     short_table = list(short_strings.sample(table_size, replace = True, 
                                             random_state = random_seed).values)
@@ -49,7 +83,7 @@ def run_benchmark(short_dataset, medium_dataset, long_dataset,
     output_table = pd.DataFrame(bench_output, columns = header)
 
     if output_file:
-        pd.to_csv(output_file, index = False)
+        output_table.to_csv(output_file, index = False)
 
     return output_table
 
@@ -73,7 +107,9 @@ def profile_runtime(table_A, table_B, tokenizer, sim_measure, num_repeat):
 def plot_benchmark(bench_output, output_file, 
                    conf_attr = 'configuration', time_attr = 'average (in secs)'):
     # Generate plot from benchmark output
-    plt.plot(bench_output[conf_attr], bench_output[time_attr], marker='o')
+    x_range = list(range(len(bench_output)))
+    plt.xticks(x_range, list(bench_output[conf_attr]))
+    plt.plot(x_range, bench_output[time_attr], marker='o')
     plt.xlabel('Configuration')
     plt.ylabel('Average time (in secs)')
     plt.title('Benchmark plot')
